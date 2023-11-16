c496d5bbcbf8cd5b68cbc33c09791d433cc9b750
Miguel <miguelnatr@gmail.com>
2023-11-16 19:31:40 +0100

    Category reorder now gets updated on the database

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
 
     const [categories, setCategories] = useState([]);
     useEffect(() => {
-        db.collection('categories').getFullList({ fields: 'order, descrip, id' }).then((res) => {
+        db.collection('categories').getFullList({ fields: 'order, descrip, id', sort: 'order' }).then((res) => {
             let loaded_categories = res.map(category => ({ data: { id: category.id, descrip: category.descrip }, id: category.order}));
             setCategories(loaded_categories);
         });
@@ -54,17 +54,32 @@ export default function Page() {
         const {active, over} = event;
         
         if (over && active.id !== over.id) {
+            let categories_to_update; 
+            if (active.id > over.id) {
+                categories_to_update = categories.filter(category => category.id >= over.id && category.id <= active.id);
+            } else {
+                categories_to_update = categories.filter(category => category.id >= active.id && category.id <= over.id);
+            }
+
             setCategories(items => {
                 const oldIndex = items.findIndex(item => item.id === active.id);
                 const newIndex = items.findIndex(item => item.id === over.id);
                 
                 return arrayMove(items, oldIndex, newIndex);
             });
+
+            categories_to_update.forEach(async category => {
+                let new_id;
+                if (category.id === active.id) new_id = over.id;
+                else if (active.id < over.id) new_id = category.id - 1;
+                else new_id = category.id + 1;
+
+                db.collection('Categories').update(category.data.id, {order: new_id});
+            });
         }
     }
 
     function deleteCategory(id, ev) {
-        debugger;
         db.collection('Categories').delete(id).then(() => {
             ev.target.closest('[role="button"]').remove();
             toast.success('Categoría eliminada', {
